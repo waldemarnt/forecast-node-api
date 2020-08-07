6834ddc659d9f7256c02eb20975e0db04e45b6c5
Douglas S. Camargo <douglas@nibbletecnologia.com.br>
2020-08-06 21:42:52 -0300

    Aula 05 - Configurando OvernightJS + Express e passando primeiro teste

---
+++ test/jest-setup.ts
@@ -0,0 +1,8 @@
+import { SetupServer } from '@src/server'
+import request from 'supertest'
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = request(server.getApp())
+})
